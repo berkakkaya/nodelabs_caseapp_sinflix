bbd9e86c0bea8ae54c813268f6d8b499bfacee34
Berk Akkaya <berkakkaya000@gmail.com>
2025-07-09 20:36:28 +0300

    feat: add color consts

---
+++ lib/core/consts/colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+const kColorBrand = Color(0xFFE50914);
+
+const kColorBlack = Color(0xFF080808);
+final kColorBlackA75 = kColorBlack.withValues(alpha: 0.75);
+
+const kColorWhite = Color(0xFFFFFFFF);
+final kColorWhiteA75 = kColorWhite.withValues(alpha: 0.75);
+final kColorWhiteA50 = kColorWhite.withValues(alpha: 0.50);
+final kColorWhiteA20 = kColorWhite.withValues(alpha: 0.20);
+final kColorWhiteA10 = kColorWhite.withValues(alpha: 0.10);
