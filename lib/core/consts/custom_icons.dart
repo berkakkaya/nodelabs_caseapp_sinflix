0db02459d639b64bbda0624e799a0ea0cca8412a
Berk Akkaya <berkakkaya000@gmail.com>
2025-07-10 00:19:22 +0300

    feat: add CustomIcons class with icon data definitions

---
+++ lib/core/consts/custom_icons.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const String _fontFamily = 'CustomIcons';
+
+  static const IconData messageOutlined = IconData(
+    0xe909,
+    fontFamily: _fontFamily,
+  );
+  static const IconData plus = IconData(0xe90a, fontFamily: _fontFamily);
+  static const IconData unlockOutlined = IconData(
+    0xe90b,
+    fontFamily: _fontFamily,
+  );
+  static const IconData addUserOutlined = IconData(
+    0xe90c,
+    fontFamily: _fontFamily,
+  );
+  static const IconData homeOutlined = IconData(
+    0xe902,
+    fontFamily: _fontFamily,
+  );
+  static const IconData arrowBack = IconData(0xe903, fontFamily: _fontFamily);
+  static const IconData hideOutlined = IconData(
+    0xe906,
+    fontFamily: _fontFamily,
+  );
+  static const IconData like = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData person = IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData diamond = IconData(0xe904, fontFamily: _fontFamily);
+  static const IconData logoApple = IconData(0xe905, fontFamily: _fontFamily);
+  static const IconData logoGoogle = IconData(0xe907, fontFamily: _fontFamily);
+  static const IconData logoFacebook = IconData(
+    0xe908,
+    fontFamily: _fontFamily,
+  );
+}
