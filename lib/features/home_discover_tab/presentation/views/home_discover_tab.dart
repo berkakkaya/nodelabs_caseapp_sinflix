171cd5fdb4fc381a5a5220783374689a38ab0b6f
Berk Akkaya <berkakkaya000@gmail.com>
2025-07-10 19:20:29 +0300

    feat: implement vertical PageView in HomeDiscoverTab with MovieView widget

--- lib/features/home_discover_tab/presentation/views/home_discover_tab.dart
+++ lib/features/home_discover_tab/presentation/views/home_discover_tab.dart
@@ -1,10 +1,26 @@
 import "package:flutter/material.dart";
+import "package:nodelabs_caseapp_sinflix/features/home_discover_tab/presentation/views/widgets/movie_view.dart";
 
 class HomeDiscoverTab extends StatelessWidget {
   const HomeDiscoverTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Home Screen"));
+    return Center(
+      child: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemBuilder: (context, index) {
+          return SafeArea(
+            child: MovieView(
+              imgProvider: NetworkImage(
+                "https://m.media-amazon.com/images/M/MV5BZWQ4MzVjMjctYzNiNy00MGQ4LWFjZWEtZjQzMmQ3NTY4NGMxXkEyXkFqcGc@._V1_FMjpg_UY4642_.jpg",
+              ),
+              title: "Test Movie",
+              description: "Test Description",
+            ),
+          );
+        },
+      ),
+    );
   }
 }
