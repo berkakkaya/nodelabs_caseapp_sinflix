cbd2f3900623cd7ec7a412bd35eb72856c6b4a87
Berk Akkaya <berkakkaya000@gmail.com>
2025-07-10 19:19:57 +0300

    feat: add kColorBlackA20 and kColorBlackGradientTTB color configs

--- lib/core/consts/colors.dart
+++ lib/core/consts/colors.dart
@@ -1,9 +1,18 @@
-import "dart:ui";
+import "package:flutter/material.dart";
 
 const kColorBrand = Color(0xFFE50914);
 
 const kColorBlack = Color(0xFF080808);
 final kColorBlackA75 = kColorBlack.withValues(alpha: 0.75);
+final kColorBlackA20 = kColorBlack.withValues(alpha: 0.20);
+
+/// A gradient that goes from black to transparent, used for the bottom
+/// of movie views.
+final kColorBlackGradientTTB = LinearGradient(
+  begin: Alignment.bottomCenter,
+  end: Alignment.topCenter,
+  colors: [kColorBlack, Colors.transparent],
+);
 
 const kColorWhite = Color(0xFFFFFFFF);
 final kColorWhiteA75 = kColorWhite.withValues(alpha: 0.75);
