a839ad022b6861dc57242b84a08e5aa4fc32abe2
Berk Akkaya <berkakkaya000@gmail.com>
2025-07-11 13:47:07 +0300

    feat: add image asset constants for UI elements

---
+++ lib/core/consts/img_assets.dart
@@ -0,0 +1,4 @@
+const String kImgArrowUp = "assets/images/arrow_up.png";
+const String kImgCoupleHearts = "assets/images/couple_hearts.png";
+const String kImgDiamond = "assets/images/diamond.png";
+const String kImgHeart = "assets/images/heart.png";
