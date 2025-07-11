62e939dbd395b5066ce73fe41cd07c764f9059db
Berk Akkaya <berkakkaya000@gmail.com>
2025-07-11 13:46:28 +0300

    feat: add color blue and gradients for offer cards

--- lib/core/consts/colors.dart
+++ lib/core/consts/colors.dart
@@ -1,10 +1,20 @@
 import "package:flutter/material.dart";
 
 const kColorBrand = Color(0xFFE50914);
+const kColorDarkRed = Color(0xFF6F060B);
 
 const kColorBlack = Color(0xFF080808);
 final kColorBlackA75 = kColorBlack.withValues(alpha: 0.75);
 final kColorBlackA20 = kColorBlack.withValues(alpha: 0.20);
+
+const kColorWhite = Color(0xFFFFFFFF);
+final kColorWhiteA75 = kColorWhite.withValues(alpha: 0.75);
+final kColorWhiteA50 = kColorWhite.withValues(alpha: 0.50);
+final kColorWhiteA20 = kColorWhite.withValues(alpha: 0.20);
+final kColorWhiteA10 = kColorWhite.withValues(alpha: 0.10);
+
+/// Color used for the best offer cards.
+final kColorBlue = Color(0xFF5949E6);
 
 /// A gradient that goes from black to transparent, used for the bottom
 /// of movie views.
@@ -14,8 +24,16 @@ final kColorBlackGradientTTB = LinearGradient(
   colors: [kColorBlack, Colors.transparent],
 );
 
-const kColorWhite = Color(0xFFFFFFFF);
-final kColorWhiteA75 = kColorWhite.withValues(alpha: 0.75);
-final kColorWhiteA50 = kColorWhite.withValues(alpha: 0.50);
-final kColorWhiteA20 = kColorWhite.withValues(alpha: 0.20);
-final kColorWhiteA10 = kColorWhite.withValues(alpha: 0.10);
+/// A gradient that used for the offer cards.
+final kColorGradientOffer = RadialGradient(
+  center: Alignment(-0.65, -0.65),
+  radius: 1.70,
+  colors: [const Color(0xFF6E050A), kColorBrand],
+);
+
+/// A gradient that used for the best offer cards.
+final kColorGradientBestOffer = RadialGradient(
+  center: Alignment(-0.65, -0.65),
+  radius: 1.70,
+  colors: [const Color(0xFF5849E6), kColorBrand],
+);
