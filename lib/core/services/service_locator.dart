0154f7ec1b0ba3f8a09687881c6396c50549643f
Berk Akkaya <berkakkaya000@gmail.com>
2025-07-12 15:04:53 +0300

    feat: update service locator to support resource mode and improve documentation

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -3,12 +3,41 @@ import "package:nodelabs_caseapp_sinflix/core/consts/api_config.dart";
 import "package:nodelabs_caseapp_sinflix/core/services/rest_api/i_rest_api_service.dart";
 import "package:nodelabs_caseapp_sinflix/core/services/rest_api/rest_api_service_factory.dart";
 
+/// Mode for serice locator resources.
+///
+/// This enum defines the mode of operation for the service locator,
+/// determining whether to use real or mock resources.
+///
+/// Currently, only [useRealResources] is implemented.
+/// [useMockResources] is a placeholder for future use.
+enum ResourceMode {
+  /// Use real resources, such as a live API and database.
+  useRealResources,
+
+  /// Use mock resources, such as a mock API and in-memory database.
+  /// This is useful for testing and development without relying on
+  /// external services.
+  ///
+  /// NOTE: This option doesn't currently have an implementation and put
+  /// as a placeholder for future use.
+  useMockResources,
+}
+
 /// Initializes the service locator with required services.
-Future<void> initializeResources() async {
+///
+/// These services include the REST API service and any other
+/// dependencies needed by the application.
+///
+/// These services are registered with the [GetIt] instance
+/// and can be accessed throughout the application using
+/// the same [GetIt] instance.
+Future<void> initializeResources({
+  ResourceMode mode = ResourceMode.useRealResources,
+}) async {
   final getIt = GetIt.instance;
 
   // Register the Dio REST API service
-  getIt.registerLazySingleton<RestApiService>(
-    () => RestApiServiceFactory.createDioService(baseUrl: kApiBaseUrl),
+  getIt.registerSingleton<RestApiService>(
+    RestApiServiceFactory.createDioService(baseUrl: kApiBaseUrl),
   );
 }
