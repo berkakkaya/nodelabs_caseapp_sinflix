5f4b8dc731269fe9f9cf97bccb4c27dbe757a996
Berk Akkaya <berkakkaya000@gmail.com>
2025-07-12 22:54:15 +0300

    feat: implement GetCurrentTokenUseCase to retrieve current authentication token

---
+++ lib/features/auth/domain/usecases/get_current_token_usecase.dart
@@ -0,0 +1,16 @@
+import "package:nodelabs_caseapp_sinflix/features/auth/domain/repositories/user_repository.dart"
+    show UserRepository;
+
+/// A use case that retrieves the current authentication token.
+///
+/// This class depends on a [UserRepository] to fetch the current token.
+/// Returns the current token as a [String] if available, or `null` if not.
+class GetCurrentTokenUseCase {
+  final UserRepository repository;
+
+  GetCurrentTokenUseCase(this.repository);
+
+  Future<String?> execute() {
+    return repository.getCurrentToken();
+  }
+}
