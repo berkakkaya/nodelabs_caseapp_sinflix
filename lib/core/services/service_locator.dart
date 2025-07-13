f88040a5053a989566b80b5d757f3e902907267c
Berk Akkaya <berkakkaya000@gmail.com>
2025-07-13 09:51:39 +0300

    feat: initialize logging service in service locator and connect other services with the new service

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,10 +1,16 @@
+import "package:dio/dio.dart";
+import "package:flutter_bloc/flutter_bloc.dart";
 import "package:get_it/get_it.dart";
 import "package:nodelabs_caseapp_sinflix/core/consts/api_config.dart";
 import "package:nodelabs_caseapp_sinflix/core/services/local_storage/i_local_storage_service.dart"
     show LocalStorageService;
 import "package:nodelabs_caseapp_sinflix/core/services/local_storage/shared_pref_imp/local_storage_shared_pref_imp.dart";
+import "package:nodelabs_caseapp_sinflix/core/services/logging/i_logging_service.dart";
+import "package:nodelabs_caseapp_sinflix/core/services/logging/talker_imp/talker_logging_service_imp.dart";
 import "package:nodelabs_caseapp_sinflix/core/services/rest_api/i_rest_api_service.dart";
 import "package:nodelabs_caseapp_sinflix/core/services/rest_api/rest_api_service_factory.dart";
+import "package:talker_bloc_logger/talker_bloc_logger.dart";
+import "package:talker_dio_logger/talker_dio_logger.dart";
 
 /// Mode for serice locator resources.
 ///
@@ -39,12 +45,32 @@ Future<void> initializeResources({
 }) async {
   final getIt = GetIt.instance;
 
+  // Initialize the logging service
+  final loggingService = TalkerLoggingService.withDefaults();
+  getIt.registerSingleton<LoggingService>(loggingService);
+
+  loggingService.i("Initializing BLoC Talker service...");
+
+  // Register BLoC observer for logging
+  Bloc.observer = TalkerBlocObserver(talker: loggingService.talker);
+
+  loggingService.i("Initializing REST API service...");
+
   // Register the Dio REST API service
+  final dio = Dio();
+  dio.interceptors.add(TalkerDioLogger(talker: loggingService.talker));
+
   getIt.registerSingleton<RestApiService>(
-    RestApiServiceFactory.createDioService(baseUrl: kApiBaseUrl),
+    RestApiServiceFactory.createDioService(dio: dio, baseUrl: kApiBaseUrl),
   );
+
+  loggingService.i("REST API service registered");
 
   // Initialize the local storage service
   final localStorageService = await LocalStorageSharedPrefsImp.withDefaults();
   getIt.registerSingleton<LocalStorageService>(localStorageService);
+
+  loggingService.i("Local storage service registered");
+
+  loggingService.i("All services initialized successfully");
 }
