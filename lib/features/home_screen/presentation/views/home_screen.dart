0bf12e1046a8024e26b1d5656094d5189ccd1841
Berk Akkaya <berkakkaya000@gmail.com>
2025-07-13 06:50:51 +0300

    feat: initialize movie bloc in home screen

--- lib/features/home_screen/presentation/views/home_screen.dart
+++ lib/features/home_screen/presentation/views/home_screen.dart
@@ -1,6 +1,17 @@
 import "package:flutter/material.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
+import "package:get_it/get_it.dart";
 import "package:nodelabs_caseapp_sinflix/core/consts/custom_icons.dart";
+import "package:nodelabs_caseapp_sinflix/core/services/rest_api/i_rest_api_service.dart";
+import "package:nodelabs_caseapp_sinflix/features/home_discover_tab/data/datasources/movie_datasource.dart";
+import "package:nodelabs_caseapp_sinflix/features/home_discover_tab/data/repositories/movie_repository.dart";
+import "package:nodelabs_caseapp_sinflix/features/home_discover_tab/domain/usecases/get_favorite_movies_usecase.dart"
+    show GetFavoriteMoviesUsecase;
+import "package:nodelabs_caseapp_sinflix/features/home_discover_tab/domain/usecases/get_movies_usecase.dart";
+import "package:nodelabs_caseapp_sinflix/features/home_discover_tab/domain/usecases/toggle_favorite_usecase.dart"
+    show ToggleFavoriteUsecase;
+import "package:nodelabs_caseapp_sinflix/features/home_discover_tab/presentation/bloc/movie_bloc.dart";
+import "package:nodelabs_caseapp_sinflix/features/home_discover_tab/presentation/bloc/movie_event.dart";
 import "package:nodelabs_caseapp_sinflix/features/home_screen/presentation/bloc/events.dart";
 import "package:nodelabs_caseapp_sinflix/features/home_screen/presentation/bloc/home_screen_viewmodel.dart";
 import "package:nodelabs_caseapp_sinflix/features/home_discover_tab/presentation/views/home_discover_tab.dart";
@@ -14,8 +25,13 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => HomeScreenViewModel(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<HomeScreenViewModel>(
+          create: (context) => HomeScreenViewModel(),
+        ),
+        BlocProvider<MovieBloc>(create: (context) => _createMovieBloc(context)),
+      ],
       child: BlocBuilder<HomeScreenViewModel, TabState>(
         builder: (context, state) {
           final bloc = context.read<HomeScreenViewModel>();
@@ -44,5 +60,18 @@ class HomeScreen extends StatelessWidget {
         },
       ),
     );
+  }
+
+  MovieBloc _createMovieBloc(BuildContext context) {
+    final apiService = GetIt.I.get<RestApiService>();
+
+    final movieDatasource = MovieDatasourceImpl(apiService: apiService);
+    final repository = DiscoverRepositoryImpl(dataSource: movieDatasource);
+
+    return MovieBloc(
+      getMoviesUsecase: GetMoviesUsecase(repository),
+      getFavoriteMoviesUsecase: GetFavoriteMoviesUsecase(repository),
+      toggleFavoriteUsecase: ToggleFavoriteUsecase(repository),
+    )..add(FetchMoviesEvent());
   }
 }
